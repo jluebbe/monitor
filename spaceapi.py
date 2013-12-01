6aa2826775b0e54154d0948e220cecee65e42fdc
Jan Luebbe <jluebbe@debian.org>
2013-12-01 20:53:18 +0100

    fix typo

--- spaceapi.py
+++ spaceapi.py
@@ -22,7 +22,7 @@ def fetch(url, timeout=10):
     if meta:
       url = urlparse.urlparse(meta['href'])
       if url.scheme == "":
-        url = urlparse.urlparse("%s://%s%s"%(baeurl.scheme, baseurl.netloc, meta['href']))
+        url = urlparse.urlparse("%s://%s%s"%(baseurl.scheme, baseurl.netloc, meta['href']))
       return fetch(url.geturl())
 
 print pretty_json(fetch("https://hickerspace.org"))
