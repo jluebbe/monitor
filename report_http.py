1aa04dc05d9c101565de66e2f8c6b7e79732e40c
Jan Luebbe <jluebbe@debian.org>
2013-12-08 19:09:40 +0100

    clean the output

--- report_http.py
+++ report_http.py
@@ -9,25 +9,30 @@ def has_hsts(headers):
     return True
 
 def report(url, headers):
-    print url, headers
+    print "  " + repr(headers)
     url = urlparse(url)
     if "location" in headers:
         location = urlparse(headers["location"])
     if url.scheme=="http":
         if headers["status"] in [301, 302] and \
             location.scheme=="https":
-            print "+1 HTTP to HTTPS redirect"
+            print " +1 HTTP to HTTPS redirect"
         else:
-            print "-1 HTTP (unencrypted)"
+            print " -1 HTTP (unencrypted)"
     elif url.scheme=="https":
-        print "+1 HTTPS (encrypted)"
+        print " +1 HTTPS (encrypted)"
         if has_hsts(headers):
-            print "+5 HSTS enabled"
+            print " +5 HSTS enabled"
 
-from orm import session, HTTPService, Result
+from orm import engine, session, HTTPService, Result
+
+engine.echo = False
 
 for x in session.query(HTTPService):
     url = x.name
-    headers = x.results.filter(Result.method=="httpservice").first().data
-    report(url, headers)
+    print url
+    result = x.results.filter(Result.method=="httpservice").first()
+    if result:
+        headers = result.data
+        report(url, headers)
 
