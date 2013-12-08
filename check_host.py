535abb18da05868341b960e347f06a65e1e53c6e
Jan Luebbe <jluebbe@debian.org>
2013-12-08 19:11:48 +0100

    check_host: use is_expired, look for the corresponding SOA

--- check_host.py
+++ check_host.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+NAME = "hostname"
+
 import dns.resolver
 from pprint import pprint
 
@@ -33,14 +35,30 @@ def find_cname(hostname):
     pass
   return result
 
+def find_soa(hostname):
+  result = {"soa": ""}
+  try:
+    query = dns.message.make_query(hostname, dns.rdatatype.SOA)
+    response = dns.query.tcp(query, resolver.nameservers[0])
+    if response.answer:
+      result["soa"] = str(response.answer[0].name)
+    elif response.authority:
+      result["soa"] = str(response.authority[0].name)
+  except dns.exception.DNSException:
+    pass
+  return result
+
 from orm import session, HostName, Result
 
 for x in session.query(HostName):
+    if not x.is_expired(NAME):
+        continue
     hostname = x.name
     data = {}
     data.update(find_address(hostname))
     data.update(find_cname(hostname))
-    x.results.append(Result("hostname", data))
-    session.commit()
+    data.update(find_soa(hostname))
+    x.results.append(Result(NAME, data))
     pprint({hostname: data})
+    session.commit()
 
