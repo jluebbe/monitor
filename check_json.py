b8c4ac23ad4751d136910d333e3edef8d98d0e97
Jan Luebbe <jluebbe@debian.org>
2013-12-08 19:15:33 +0100

    check_json: add json downloader

---
+++ check_json.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import requests
+import json
+import urlparse
+
+def fetch(url, timeout=10):
+  assert(isinstance(url, basestring))
+  url = urlparse.urlparse(url)
+  page = requests.get(url.geturl(), timeout=timeout)
+  try:
+    return json.loads(page.content)
+  except ValueError:
+    return {}
+
+from orm import session, JSONAPI, Result
+
+import sqlalchemy
+
+for x in session.query(JSONAPI):
+    x.results.append(Result("jsonapi", fetch(x.name)))
+    session.commit()
+
