b02fedaf79b160b702999f97058daac53475c9e7
Jan Luebbe <jluebbe@debian.org>
2014-03-16 22:05:10 +0100

    discover_spaceapi: add support for feeds, email and twitter
    Also try a different approach to updating the list of children.

--- discover_spaceapi.py
+++ discover_spaceapi.py
@@ -3,26 +3,64 @@
 from urlparse import urlparse
 
 from orm import session, SpaceAPI, Result
-from orm import HTTPService, HostName
+from orm import HTTPService, HostName, Feed, EMailAddress
 
 def host_from_url(url):
-  assert(isinstance(url, basestring))
-  return urlparse(url).hostname
+    assert(isinstance(url, basestring))
+    return urlparse(url).hostname
+
+def discover_feeds(c, feeds):
+    if "name" in feeds and "url" in feeds:
+        n = Feed(name=feeds["url"])
+        n.conf["kind"] = feeds["name"]
+        c.add(n)
+    elif isinstance(feeds, list): # list of feeds
+        for props in feeds:
+            n = Feed(name=props["url"])
+            print n.conf
+            n.conf["kind"] = props["name"]
+            c.add(n)
+    elif isinstance(feeds, dict): # dict of feeds
+        for kind, props in feeds.items():
+            n = Feed(name=props["url"])
+            print props
+            n.conf["kind"] = kind
+            c.add(n)
+
+def discover_contacts(c, contacts):
+    if "email" in contacts:
+        n = EMailAddress(name=contacts["email"])
+        n.conf["kind"] = "primary contact"
+        c.add(n)
+    if "ml" in contacts:
+        try:
+            n = EMailAddress(name=contacts["ml"])
+            n.conf["kind"] = "primary mailing list"
+            c.add(n)
+        except ValueError:
+            pass
+    if "twitter" in contacts:
+        n = Feed(name="https://twitter.com/%s" % contacts["twitter"].lstrip("@"))
+        n.conf["kind"] = "primary twitter feed"
+        c.add(n)
 
 def discover(node, data):
     print node
     c = node.children["spaceapi"]
-    existing = set((x.__class__, x.name) for x in c)
-    print existing
-    current = set()
+    while c:
+        c.pop()
+    c = set()
     if "url" in data:
-        current.add((HTTPService, data["url"]))
+        c.add(HTTPService(name=data["url"]))
         host = host_from_url(data["url"])
         if not host is None:
-            current.add((HostName, host))
-    new = current - existing
-    for x in new:
-        c.append(x[0](name=x[1]))
+            c.add(HostName(name=host))
+    if "feeds" in data:
+        discover_feeds(c, data["feeds"])
+    if "contact" in data:
+        discover_contacts(c, data["contact"])
+    for n in c:
+        node.children["spaceapi"].append(n)
 
 for node in session.query(SpaceAPI):
     result = node.results.filter(Result.method=="spaceapi").first()
